9acc06bdcabf2bdc3203d7c8c12cf9e469ebecbb
rhmanovic <31311273+rhmanovic@users.noreply.github.com>
2024-09-13 23:31:18 +0300

    Update product.js

--- public/js/product.js
+++ public/js/product.js
@@ -1,60 +1,81 @@
-// Function to open the delete confirmation modal with product details
-function DeleteModalTrigger(productId, productName) {
-  document.getElementById('productToDelete').textContent = productName;
-  document.getElementById('productToDeleteID').value = productId;
-  $('#deleteConfirmationModal').modal('show');
-}
+var mongoose = require("mongoose");
+var bcrypt = require("bcryptjs");
 
+// Counter Schema to keep track of the last number used for product IDs
+var CounterSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
+  seq: { type: Number, default: 0 }
+});
 
-// Add event listener for all product switches
-document.addEventListener('change', async (event) => {
-  if (event.target.classList.contains('product-switch')) {
-    const productId = event.target.dataset.id; // Get the product ID from the data-id attribute
-    const newStatus = event.target.checked; // Get the new status from the checked property of the switch
+var Counter = mongoose.model('Counter', CounterSchema);
 
+// Variation Schema as a sub-document
+var VariationSchema = new mongoose.Schema({
+  v_name_ar: { type: String, required: true },
+  v_name_en: { type: String, required: true },
+  v_sale_price: { type: Number, required: true },
+  v_purchase_price: { type: Number },
+  v_available_quantity: { type: Number , default: 0 },
+  v_warehouse_stock: { type: Number, default: 0 }, // Added warehouse stock field
+  v_barcode: { type: String },
+  v_purchase_limit: { type: Number, default: null },
+  v_brand: { type: String, default: '' },  // Added brand name field
+  v_warranty: { type: String, default: '' } // Added warranty field
+});
+
+// Main Product Schema
+var ProductSchema = new mongoose.Schema({
+  merchant: { type: mongoose.Schema.Types.ObjectId, ref: 'Merchant' },
+  product_number: { type: Number }, // field for the unique product number
+  category_number: { type: Number }, // field for the unique product number
+  product_name_en: { type: String, required: true },
+  product_name_ar: { type: String, required: true },
+  category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' },
+  brand: { type: String, default: '' }, // Added brand name field
+  warranty: { type: String, default: '' }, // Added warranty field
+  order_command: { type: Number, default: 0 },
+  weight: { type: String, default: 0 },
+  keywords: { type: String, default: '' },
+  product_image: { type: String, default: '' },
+  product_images: [{ type: String }], // Updated to store multiple images
+  youtube_video_id: { type: String, default: '' },
+  description_ar: { type: String, default: '' },
+  description_en: { type: String, default: '' },
+  sale_price: { type: Number },
+  purchase_price: { type: Number },
+  purchase_limit: { type: Number, default: null },
+  barcode: { type: String, default: '' },
+  pdt_discount_type: { type: String, enum: ['percentage', 'quantity', 'nodiscount'] },
+  pdt_discount: { type: Number, min: 0, max: 99 },
+  variations: [VariationSchema],
+  Stock: { type: Number, default: 0 },
+  warehouse_stock: { type: Number, default: 0 },
+  options: { type: Boolean, default: false }, // Example boolean field
+  standard_sizes: { type: Boolean, default: false }, // Standard sizes field as a boolean
+  status: {
+    type: Boolean,
+    required: true,
+    default: true // Automatically sets status to true unless specified otherwise
+  },
+  product_type: { type: String, enum: ['Simple', 'Complex'] , default: 'Simple'},
+});
+
+// Pre-save middleware to auto-increment the product_number only on creation
+ProductSchema.pre('save', async function(next) {
+  if (this.isNew) { // Check if the document is new
+    var doc = this;
+    var counterId = `productNumber_${doc.merchant}`;
     try {
-      // Disable the switch element and add a class to set opacity to the parent label element
-      event.target.disabled = true;
-      const labelElement = event.target.closest('label');
-      labelElement.classList.add('disabled-opacity');
-
-      console.log("labelElement.classList:: " + labelElement.classList)
-
-      // Send a POST request to update the product status
-      const response = await fetch('/manager/product/update-status', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ id: productId, status: newStatus })
-      });
-
-      if (!response.ok) {
-        throw new Error('Failed to update product status');
-      }
-
-      // Update the product status on the frontend if the request is successful
-      const productRow = event.target.closest('tr');
-      productRow.querySelector('.checkmark').style.display = newStatus ? 'block' : 'none'; // Show/hide the checkmark span based on the new status
-
-      // Show toast notification
-      const toast = new bootstrap.Toast(document.querySelector('.toast'));
-       $('#toastBody').text("Product updated successfully");
-      toast.show();
-
-      // Re-enable the switch element after 8 seconds and remove the opacity class
-      setTimeout(() => {
-        labelElement.classList.remove('disabled-opacity');
-        event.target.disabled = false;
-
-      }, 5000);
+      var cnt = await Counter.findOneAndUpdate({ _id: counterId }, { $inc: { seq: 1 } }, { new: true, upsert: true });
+      doc.product_number = cnt.seq;
+      next();
     } catch (error) {
-      console.error('Error updating product status:', error);
-      event.target.disabled = false;
-      // Re-enable the switch element and remove the opacity class in case of error
-      labelElement.classList.remove('disabled-opacity');
+      return next(error);
     }
+  } else {
+    next(); // If not new, just continue to save without incrementing
   }
 });
 
-
+var Product = mongoose.model("product", ProductSchema);
+module.exports = Product;
